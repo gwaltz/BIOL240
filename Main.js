6a3d524ff64b3143a4c5270f7218fb0a8009eaa1
gwaltz <gwaltz@sandiego.edu>
2023-05-01 11:01:57 -0700

    JPG

--- Main.js
+++ Main.js
@@ -1,2 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var myButton = document.getElementById("my-button");
+    myButton.addEventListener("click", function() {
+      alert("Button clicked!");
+    });
 
+    var toggleButton = document.getElementById("toggle-button");
+    var additionalInfoElement = document.getElementById("additional-info");
 
+    toggleButton.addEventListener("click", function() {
+    if (additionalInfoElement.style.display === "none") {
+        additionalInfoElement.style.display = "block";
+        toggleButton.textContent = "Hide additional information";
+    } else {
+        additionalInfoElement.style.display = "none";
+        toggleButton.textContent = "Show additional information";
+    }});
+
+});
