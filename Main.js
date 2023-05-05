66c60ed675978733b80ea43c708ba17ca9d235e2
gwaltz <gwaltz@sandiego.edu>
2023-05-05 08:53:52 -0700

    button function

--- Main.js
+++ Main.js
@@ -1,18 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var myButton = document.getElementById("my-button");
-    myButton.addEventListener("click", function() {
-      alert("Button clicked!");
+    myButton.addEventListener("click", function () {
+        alert("Button clicked!");
     });
-
-    var toggleButton = document.getElementById("toggle-button");
-    var additionalInfoElement = document.getElementById("additional-info");
-
-    toggleButton.addEventListener("click", function() {
-    if (additionalInfoElement.style.display === "none") {
-        additionalInfoElement.style.display = "block";
-        toggleButton.textContent = "Close";
-    } else {
-        additionalInfoElement.style.display = "none";
-        toggleButton.textContent = "More";
-    }});
 });
+
+function toggleInfo(id) {
+    var toggleButton = document.getElementById("toggle-button-" + id);
+    var additionalInfoElement = document.getElementById("additional-info-" + id);
+    toggleButton.addEventListener("click", function () {
+        if (additionalInfoElement.style.display === "none") {
+            additionalInfoElement.style.display = "block";
+            toggleButton.textContent = "Close";
+        } else {
+            additionalInfoElement.style.display = "none";
+            toggleButton.textContent = "More";
+        }
+    })
+}
